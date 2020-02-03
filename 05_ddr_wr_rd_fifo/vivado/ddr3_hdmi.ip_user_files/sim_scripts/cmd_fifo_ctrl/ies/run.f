74cae9bf59cea9b8956a3b2e1eefc9782b68e3fb
sinply <2322900041@qq.com>
2020-02-03 19:11:40 +0800

    高级课程第5次 跟随老师做项目 DDR3添加fifo实现读写控制

---
+++ 05_ddr_wr_rd_fifo/vivado/ddr3_hdmi.ip_user_files/sim_scripts/cmd_fifo_ctrl/ies/run.f
@@ -0,0 +1,23 @@
+-makelib ies_lib/xil_defaultlib -sv \
+  "D:/ProgramData/Xilinx/Vivado/2018.2/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+  "D:/ProgramData/Xilinx/Vivado/2018.2/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib ies_lib/xpm \
+  "D:/ProgramData/Xilinx/Vivado/2018.2/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies_lib/fifo_generator_v13_2_2 \
+  "../../../ipstatic/simulation/fifo_generator_vlog_beh.v" \
+-endlib
+-makelib ies_lib/fifo_generator_v13_2_2 \
+  "../../../ipstatic/hdl/fifo_generator_v13_2_rfs.vhd" \
+-endlib
+-makelib ies_lib/fifo_generator_v13_2_2 \
+  "../../../ipstatic/hdl/fifo_generator_v13_2_rfs.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../../ddr3_hdmi.srcs/sources_1/ip/cmd_fifo_ctrl/sim/cmd_fifo_ctrl.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
